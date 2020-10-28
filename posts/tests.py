caf8500d90708ef8a15107983eb53cecce53563c
Amirhossein Andohkosh <4315615+amirha97@users.noreply.github.com>
2020-10-28 09:48:09 +0000

    Add post model
    Additionally register model for admin site

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from .models import Post
+
+
+class BlogTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        test_user_1 = User.objects.create_user(username='t1', password='p1')
+        test_user_1.save()
+
+        test_post = Post.objects.create(
+            author=test_user_1,
+            title='post title',
+            body='post body'
+        )
+        test_post.save()
+
+    def test_blog_content(self):
+        post = Post.objects.get(id=1)
+        self.assertEqual(post.author.username, 't1')
+        self.assertEqual(post.title, 'post title')
+        self.assertEqual(post.body, 'post body')
