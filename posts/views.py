5c9486e322160c7ecd437daa8cbbab57b447e68b
Amirhossein Andohkosh <4315615+amirha97@users.noreply.github.com>
2020-10-28 19:09:12 +0000

    Add post creation validation to only allow post creation when the author in the request is same as the caller

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.exceptions import ValidationError
 
 from .models import Post
 from .permissions import IsAuthorOrReadOnly
@@ -8,6 +9,11 @@ from .serializers import PostSerializer
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        if self.request.user.id != serializer.validated_data['author'].id:
+            raise ValidationError('You can only create posts for yourself.')
+        serializer.save()
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
