aea2e626712fcef3394e557914b08711898de1ab
Amirhossein Andohkosh <4315615+amirha97@users.noreply.github.com>
2020-10-28 18:08:51 +0000

    Add custom permission for individual post endpoint to only allow updates/deletion if the caller is the posts author

---
+++ posts/permissions.py
@@ -0,0 +1,11 @@
+from rest_framework import permissions
+
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        #  read-only permissions granted for any request
+        # write permissions are only granted if the caller is the author
+        return bool(
+            request.method in permissions.SAFE_METHODS or
+            obj.author == request.user
+        )
