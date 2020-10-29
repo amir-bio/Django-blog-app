f68e5a4fb663935f7c4f39daaac6c31a61a71086
Amirhossein Andohkosh <4315615+amirha97@users.noreply.github.com>
2020-10-29 12:49:09 +0000

    Move API doc endpoint definitions to project level
    This change makes it easier to reason about how all other endpoints
    including ones outside the posts app are included

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
+from rest_framework.schemas import get_schema_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
@@ -23,4 +25,16 @@ urlpatterns = [
     path('api/v1/dj-rest-auth/', include('dj_rest_auth.urls')),
     path('api/v1/dj-rest-auth/registration/',
          include('dj_rest_auth.registration.urls')),
+        # path to dynamic OpenAPI schema (used in redoc path)
+    path('api/v1/openapi/', get_schema_view(
+        title="My Blog",
+        description="Small blog project",
+        version="1.0.0"
+    ), name='openapi-schema'),
+
+    # redoc documentation for the API
+    path('api/v1/redoc/', TemplateView.as_view(
+        template_name='posts/redoc.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='redoc'),
 ]
--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,4 @@
-from django.views.generic import TemplateView
 from django.urls import path
-
-from rest_framework.schemas import get_schema_view
 
 from .views import PostList, PostDetail
 
@@ -9,17 +6,4 @@ from .views import PostList, PostDetail
 urlpatterns = [
     path('', PostList.as_view()),
     path('<int:pk>/', PostDetail.as_view()),
-
-    # path to dynamic OpenAPI schema (used in redoc path)
-    path('openapi/', get_schema_view(
-        title="My Blog",
-        description="Small blog project",
-        version="1.0.0"
-    ), name='openapi-schema'),
-
-    # redoc documentation for the API
-    path('redoc/', TemplateView.as_view(
-        template_name='posts/redoc.html',
-        extra_context={'schema_url': 'openapi-schema'}
-    ), name='redoc'),
 ]
